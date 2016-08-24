893c37dc9223d391da6b9683063082971190587d
Pascal Aubort <pascal.aubort@gmail.com>
2016-08-24 05:57:28 +0000

    more work on the contact form and add recaptcha

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import ( 
     "net/http"
+    "fmt"
     
     "appengine"
+    "appengine/mail"
+    
+    "github.com/haisum/recaptcha"
+    // "appengine/log"
 )
 
 func init() {
@@ -12,8 +17,36 @@ func init() {
 }
 
 func handleContactus(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    content := r.FormValue("firstname")
-    c.Debugf("Form: %s", content)
+    
+    ctx := appengine.NewContext(r)
+    
+	re := recaptcha.R{
+		Secret: "6LeURygTAAAAAApwnTDNSQ7fSHAf5vfCPCPuAWlJ",
+	}
+    
+    isValid := re.Verify(*r)
+	if isValid {
+		ctx.Infof("Valid")
+	} else {
+		ctx.Errorf("Invalid! These errors ocurred: %v", re.LastError())
+	}
+
+    
+    name := r.FormValue("name")
+    receiver := r.FormValue("email")
+    request := r.FormValue("request")
+    
+    msg := &mail.Message{
+		Sender:  "Loyall.ch Info <info@loyall.ch>",
+		To:      []string{receiver},
+		Subject: "Your request to Loyall",
+		Body:    fmt.Sprintf("Dear %v %v", name, request),
+	}
+	
+	if err := mail.Send(ctx, msg); err != nil {
+	    ctx.Errorf("Couldn't send email: %v", err)
+	} else {
+	    ctx.Infof("An email has been sent to: %v", receiver)
+	}
     return
 }
--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@
  */
 $("#contact-form").submit(function(e) {
     var url = "/contactus/";
-    var formData = $("#contact-form").serializeObject();
+    var formData = $("#contact-form").serialize();
     
     $.ajax({
           type: "POST",
@@ -20,19 +20,19 @@ $("#contact-form").submit(function(e) {
 });
 
 
-$.fn.serializeObject = function()
-{
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
-};
+// $.fn.serializeObject = function()
+// {
+//     var o = {};
+//     var a = this.serializeArray();
+//     $.each(a, function() {
+//         if (o[this.name] !== undefined) {
+//             if (!o[this.name].push) {
+//                 o[this.name] = [o[this.name]];
+//             }
+//             o[this.name].push(this.value || '');
+//         } else {
+//             o[this.name] = this.value || '';
+//         }
+//     });
+//     return o;
+// };
