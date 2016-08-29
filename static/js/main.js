22ec60babe66b0e3753a8c28928d28ad12060c55
Pascal Aubort <pascal.aubort@gmail.com>
2016-08-29 12:48:28 +0000

    Add form validation

--- static/js/main.js
+++ static/js/main.js
@@ -3,8 +3,12 @@
  * Handles the contact form submit
  */
 $("#contact-form").submit(function(e) {
+  e.preventDefault(); // avoid to execute the actual submit of the form.
     var url = "/contactus/";
     var formData = $("#contact-form").serialize();
+    if (validateForm(formData)){
+      return;
+    }
     
     $.ajax({
           type: "POST",
@@ -19,11 +23,25 @@ $("#contact-form").submit(function(e) {
           error: function(data){
             //console.log("ERROR");
           }
-         });
+        });
     
-    e.preventDefault(); // avoid to execute the actual submit of the form.
+    
 });
 
 var verifyRecaptchaCallback = function(response) {
   $("#submit-button").prop('disabled', false);
 };
+
+var validateForm = function(formData){
+  
+  var isDirty = false;
+  var inputs = document.querySelectorAll(".mdl-textfield");
+    for (var i = 0, l = inputs.length; i < l; i++) {
+      if(inputs[i].MaterialTextfield.input_.value == ""){
+        inputs[i].classList.add("is-invalid", "is-dirty");
+        isDirty = true;
+      }
+    }
+  
+  return isDirty;
+}
