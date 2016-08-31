69a4d5e39de8f5e7cd0f9d74a6d0ccb13cbf20cf
Pascal Aubort <pascal.aubort@gmail.com>
2016-08-31 08:57:57 +0000

    Refactor ticket creation and fix quote issue

--- main.go
+++ main.go
@@ -2,35 +2,35 @@ package main
 
 import ( 
     "net/http"
-    // "fmt"
-    
- //   //recaptcha imports
- //   "encoding/json"
-	"io/ioutil"
-	// //"net/http"
-	// "net/url"
-	// "time"
-	"strings"
-    
+    "encoding/json"
+	
+    "bytes"
     
     "appengine"
-    // "appengine/mail"
     "appengine/urlfetch"
-    
-    // "./recaptcha"
-    // "appengine/log"
 )
 
 var (
     grooveApiKey = "31ec9b652af2605b87e51ca4acaed7e34ab2274cd588e5ab6fe4afe233816cdf"
 )
 
-func init() {
-	http.Handle("/", http.FileServer(http.Dir("public")))
-	http.HandleFunc("/contactus/", handleContactus)
+type GrooveTicket struct {
+		From string                 `json:"from"`
+		To string                   `json:"to"`
+		Subject string              `json:"subject"`
+		Name string                 `json:"name"`
+		Email string                `json:"email"`
+		Body string                 `json:"body"`
+		SendCopyToCustomer bool     `json:"send_copy_to_customer"`
 }
 
-func handleContactus(w http.ResponseWriter, r *http.Request) {
+
+func init() {
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/contactus/", HandleContactus)
+}
+
+func HandleContactus(w http.ResponseWriter, r *http.Request) {
     
     c := appengine.NewContext(r)
     
@@ -38,10 +38,41 @@ func handleContactus(w http.ResponseWriter, r *http.Request) {
     receiver := r.FormValue("email")
     request := r.FormValue("request")
     
-    //request = strings.Replace(request, "`", "'", -1)
-    request = strings.Replace(request, "\"", "'", -1)
-    
     c.Infof("Received values from form submit name: %v, email: %v, request: %v", name, receiver, request)
+    
+    ticket := GrooveTicket{receiver, "info@loyall.ch", "Your message to Loyall", name, receiver, request, true}
+    b := new(bytes.Buffer)
+    json.NewEncoder(b).Encode(ticket)
+    
+    req, err := http.NewRequest("POST", "https://api.groovehq.com/v1/tickets", b)
+    
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    
+    req.Header.Set("Authorization", "Bearer 31ec9b652af2605b87e51ca4acaed7e34ab2274cd588e5ab6fe4afe233816cdf")
+	req.Header.Set("Content-Type", "application/json")
+	
+	client := urlfetch.Client(c)
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	
+	if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    
+    if resp.StatusCode != 201 {
+        http.Error(w, "Error while creating ticket:" + resp.Status, resp.StatusCode)
+        c.Errorf("Error while creating ticket: %v", resp.Status)
+        return
+    }
+    
+    // body, _ := ioutil.ReadAll(resp.Body)
+    // c.Debugf(string(body))
+    
+    c.Infof("Groove ticket creation responded with: %s", resp.Status)
     
 //     msg := &mail.Message{
 // 		Sender:  "Loyall.ch Info <info@loyall.ch>",
@@ -59,7 +90,7 @@ func handleContactus(w http.ResponseWriter, r *http.Request) {
 // 	}
     // http.Redirect(w, r, "http://www.google.com", 301)
     
-    createGrooveTicket(w, c, receiver, request, name)	
+    // createGrooveTicket(w, c, receiver, request, name)	
     
 }
 
@@ -71,47 +102,53 @@ func createGrooveTicket(w http.ResponseWriter, c appengine.Context, sender strin
     //resp, err := client.Post("https://api.groovehq.com/v1/me?access_token=31ec9b652af2605b87e51ca4acaed7e34ab2274cd588e5ab6fe4afe233816cdf", "application/json", nil)
     //var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
     
-    json := `{"body":"`+request+`",
-        "from":"`+sender+`",
-        "to":"info@loyall.ch", 
-        "subject":"Your message to Loyall",
-        "name":"`+name+`",
-        "email": "`+sender+`",
-        "send_copy_to_customer": true,
-        "body": "`+request+`"}`
     
-    c.Infof("Trying to create a Groove Ticket with following input:%v", json)
     
-    buf := strings.NewReader(json)
+    // ticket := GrooveTicket{sender, "info@loyall.ch", "Your message to Loyall", name, sender, request, true}
+    // b := new(bytes.Buffer)
+    // json.NewEncoder(b).Encode(ticket)
     
-    req, err := http.NewRequest("POST", "https://api.groovehq.com/v1/tickets", buf)
+    // req, err := http.NewRequest("POST", "https://api.groovehq.com/v1/tickets", b)
     
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
     
-	req.Header.Set("Authorization", "Bearer 31ec9b652af2605b87e51ca4acaed7e34ab2274cd588e5ab6fe4afe233816cdf")
-	req.Header.Set("Content-Type", "application/json")
+    // json := `{"body":"`+request+`",
+    //     "from":"`+sender+`",
+    //     "to":"info@loyall.ch", 
+    //     "subject":"Your message to Loyall",
+    //     "name":"`+name+`",
+    //     "email": "`+sender+`",
+    //     "send_copy_to_customer": true,
+    //     "body": "`+request+`"}`
+    
+    // c.Infof("Trying to create a Groove Ticket with following input:%v", json)
+    
+    // 
+    // if err != nil {
+    //     http.Error(w, err.Error(), http.StatusInternalServerError)
+    //     return
+    // }
+    
+// 	req.Header.Set("Authorization", "Bearer 31ec9b652af2605b87e51ca4acaed7e34ab2274cd588e5ab6fe4afe233816cdf")
+// 	req.Header.Set("Content-Type", "application/json")
 	
-	client := urlfetch.Client(c)
-	resp, err := client.Do(req)
+// 	client := urlfetch.Client(c)
+// 	resp, err := client.Do(req)
 	
-    defer resp.Body.Close()
+//     defer resp.Body.Close()
     
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+    // if err != nil {
+    //     http.Error(w, err.Error(), http.StatusInternalServerError)
+    //     return
+    // }
     
-    if resp.StatusCode != 201 {
-        http.Error(w, "Error while creating ticket:" + resp.Status, resp.StatusCode)
-        c.Errorf("Error while creating ticket: %v", resp.Status)
-        return
-    }
+    // if resp.StatusCode != 201 {
+    //     http.Error(w, "Error while creating ticket:" + resp.Status, resp.StatusCode)
+    //     c.Errorf("Error while creating ticket: %v", resp.Status)
+    //     return
+    // }
     
-    body, _ := ioutil.ReadAll(resp.Body)
-    c.Debugf(string(body))
+    // body, _ := ioutil.ReadAll(resp.Body)
+    // c.Debugf(string(body))
     
-    c.Infof("Groove ticket creation responded with: %s", resp.Status)
+    // c.Infof("Groove ticket creation responded with: %s", resp.Status)
 }
---
+++ main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    // "testing"
+    // "net/url"
+    // "net/http"
+    // "strings"
+    // "fmt"
+)
+
+const DefaultRemoteAddr = "0.0.0.0"
+
+func TestHandleContactus(t *testing.T) {
+    
+//     hc := http.Client{}
+//     // req, err := http.NewRequest("POST", APIURL, nil)
+
+//     form := url.Values{}
+//     form.Add("name", "Pascal")
+// 	form.Add("receiver", "pascal.aubort@gmail.com")
+// 	form.Add("request", "test")
+	
+// 	url := "http://0.0.0.0:8000/contactus/"
+// 	req, err := http.NewRequest("POST", url, strings.NewReader(form.Encode()))
+	
+//     req.PostForm = form
+//     req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+//     // glog.Info("form was %v", form)
+//     resp, err := hc.Do(req)
+    
+//     if err != nil {
+//         t.Fatal(err.Error())
+//     }
+    
+//     t.Log(resp.Status)
+}
